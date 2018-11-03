eb2797f747cd0bfd4109b67b1c65f005e703fa47
gbox3d <gbox3d@gmail.com>
2018-11-04 00:48:19 +0900

    first upload

--- readme.md
+++ readme.md
@@ -1,7 +1,19 @@
 mongoPi
 ====
 
-몽고디비 이잰 우리손으로 직접 컴파일해서 만들어보자 
+언제까지 남이 떠준 밥만 먹고살것인가?  
+이제부터는 우리스스로 밥을 떠먹자    
+여기서 몽고디비를 컴파일하라.
+
+라즈베리파이에서 몽고 디비를 컴파일해서 써본 것 정리한다.
+
+clone 후 npm run build 로 간편하게 스크립트 실행시켜 컴파일환경 세팅부터 빌드까지 한방에 처리한다.
+
+```
+git clone https://github.com/gbox3d/mongoPi.git
+cd mongoPi
+npm run build
+```
 
 
 ## 참고자료
--- run.sh
+++ run.sh
@@ -20,3 +20,10 @@ wget https://github.com/mongodb/mongo-c-driver/archive/1.13.0.tar.gz
 tar xzvf 1.13.0.tar.gz
 cd mongo-c-driver-1.13.0/
 cmake . && make install
+
+echo "== build mongodb =="
+
+tar xzvf mongodb-src-r4.0.3.tar.gz
+cd mongodb-src-r4.0.3
+
+./buildscripts/scons.py --link-model=dynamic --install-mode=hygienic --disable-warnings-as-errors --enable-free-mon=off --js-engine=none --dbg=off --wiredtiger=off --use-system-mongo-c=on --allocator=system CPPPATH="/usr/local/include/libbson-1.0 /usr/local/include/libmongoc-1.0" LIBPATH="/usr/local/lib" CCFLAGS="-mabi=aapcs-linux -march=armv7-a" install-embedded-{dev,test} -j1
